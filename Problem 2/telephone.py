12070d155ac5aef2e36b69cb9be51217789a6ab1
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-02-01 14:25:26 -0600

    problem 2

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,8 @@
 def telephone():
     phone_number = int(input())
-    ''' Type your code here. '''
+    phone_acode = str(phone_number // 10000000)
+    phone_middle = str((phone_number // 10000) % 1000)
+    phone_end = str(phone_number % 10000)
+    print('('+phone_acode+') '+phone_middle+'-'+phone_end)
 if __name__ == "__main__":
     telephone()
